9107a8cc0580b9800a810cba58e25a52a9fce415
Diego Burlando <diego@dmm888.com>
2018-01-06 19:01:11 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+//  OpenShift sample Node application
+let express = require('express');
+let app = express();    
+
+let port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+let ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
+
+
+
+app.get('/', function (req, res) {
+  
+      res.send('hello');
+ 
+});
+
+
+app.listen(port, ip);
+console.log('Server running on http://%s:%s', ip, port);
+
+module.exports = app ;
